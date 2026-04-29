866bdf1e50908d85c4be06aa51f97a3eddaa9edd
leojmont <leoneljesusmontenegro@gmail.com>
2026-04-28 21:02:57 -0300

    Add files via upload

---
+++ pruebasitio/scripts/main.js
@@ -0,0 +1,54 @@
+const listItems = document.querySelectorAll("li");
+
+function toggleDone(e) {
+    if (!e.target.className) {
+        e.target.className = "done";
+    } else {
+        e.target.className = "";
+    }
+}
+
+listItems.forEach((item) => {
+    item.addEventListener("click", toggleDone);
+});
+
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/mora.JPG") {
+        myImage.setAttribute("src", "images/ampi.JPG");
+    } else {
+        myImage.setAttribute("src", "images/mora.JPG");
+    }
+});
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.addEventListener("click", () => {
+    setUserName();
+});
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+}
